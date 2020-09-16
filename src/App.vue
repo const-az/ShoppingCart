ebc142dc669c7e8888a6215fa409be2fcf272a58
Constanza Avilés Zavando <c.avileszavando@gmail.com>
2020-09-16 02:05:16 -0300

    NavBar.vue : design done. App.vue : Product list done. / No functions done yet.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <nav-bar></nav-bar>
+    <v-main class="my-10">
+      <v-container>
+        <p class="text-h5 text-center font-weight-medium mt-3">Productos disponibles</p>
+        <v-card flat class="my-8" v-for="(product, index) in products" :key="index">
+          <v-row>
+            <v-col cols="12" md="10">
+              <p class="text-h5 black--text">{{product.name}} <span class="mx-3 text-body-1">{{product.price}}</span></p>
+            </v-col>
+            <v-col cols="12" md="2" class="text-right">
+              <v-btn text color="deep-purple accent-4">
+                <span class="hidden-md-and-up mr-3">Agregar</span><v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    NavBar
+  },
+  data: () => ({
+    products: [
+      { name: 'Caja de frutas', price: "$1.200"},
+      { name: 'Tabla de quesos', price: "$10.700"},
+      { name: 'Pizza congelada', price: "$5.200"},
+      { name: 'Pack bebidas', price: "$3.900"},
+      { name: 'Ensalada surtida', price: "$1.200"},
+    ]
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app-bar color="transparent" dense flat absolute class="px-5 py-2">
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon color="deep-purple accent-4">mdi-cart-outline</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
